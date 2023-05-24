54620f890d80df876ba4f365a52a2c3acd732635
Matej Šimák <131687569+NoxiousHex@users.noreply.github.com>
2023-05-24 22:37:22 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ function App() {
 
     return (
         <>
-            <Header setPage={setPage} />
+            <Header page={page} setPage={setPage} />
             <main>{renderRoute}</main>
         </>
     );

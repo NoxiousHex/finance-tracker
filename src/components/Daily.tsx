0504d64f6f47f4a3653666166a2b8380b2a528ce
NoxiousHex <131687569+NoxiousHex@users.noreply.github.com>
2023-06-11 18:43:47 +0200

    Fix storing and parsing history

--- src/components/Daily.tsx
+++ src/components/Daily.tsx
@@ -2,273 +2,220 @@ import { FC, useState, useEffect } from "react";
 import RenderGraph from "./Graph";
 import { parseFinanceObject } from "../utils/currencies";
 import {
-    CurrencyObject,
-    ActiveFinanceObject,
-    storedFinanceObject,
-    GraphText,
+	CurrencyObject,
+	ActiveFinanceObject,
+	storedFinanceObject,
+	GraphText,
 } from "../utils/interfaces";
 import {
-    constructEmptyFinance,
-    decimalConv,
-    timeDiffInDays,
-    last,
+	constructEmptyFinance,
+	decimalConv,
+	timeDiffInDays,
+	last,
 } from "../utils/utils";
 import "../styles/daily.css";
 import { addDoc, onSnapshot, setDoc, doc } from "firebase/firestore";
 import { dailyCollection, historyCollection } from "../firebase";
 
 interface DailyProps {
-    history: storedFinanceObject[];
-    addToHistory: (
-        income: number,
-        spending: number,
-        balance: number,
-        date: string
-    ) => void;
-    currency: CurrencyObject;
+	history: storedFinanceObject[];
+	addToHistory: (
+		income: number,
+		spending: number,
+		balance: number,
+		date: string
+	) => void;
+	currency: CurrencyObject;
 }
 
 const Daily: FC<DailyProps> = (props) => {
-    const { history, addToHistory, currency } = props;
+	const { history, addToHistory, currency } = props;
 
-    const [daily, setDaily] = useState<ActiveFinanceObject>(
-        constructEmptyFinance(currency)
-    );
+	const [daily, setDaily] = useState<ActiveFinanceObject>(
+		constructEmptyFinance(currency)
+	);
 
-    const { income, balance, spending, date } = daily;
+	const { income, balance, spending, date } = daily;
 
-    const [dailyId, setId] = useState<string>("");
+	const [dailyId, setId] = useState<string>("");
 
-    const [input, setInput] = useState<string>("");
+	const [input, setInput] = useState<string>("");
 
-    useEffect(() => {
-        const unsubscribe = onSnapshot(dailyCollection, (snapshot) => {
-            const dailyArr: storedFinanceObject = snapshot.docs.map((doc) => {
-                const data = doc.data();
-                const id = doc.id;
-                setId(id);
-                return {
-                    income: data.income,
-                    balance: data.balance,
-                    spending: data.spending,
-                    date: data.date,
-                };
-            })[0];
-            if (dailyArr) {
-                setDaily(parseFinanceObject(dailyArr, currency));
-            }
-        });
-        return () => {
-            unsubscribe();
-        };
-    }, [input]);
+	useEffect(() => {
+		const unsubscribe = onSnapshot(dailyCollection, (snapshot) => {
+			const dailyArr: storedFinanceObject = snapshot.docs.map((doc) => {
+				const data = doc.data();
+				const id = doc.id;
+				setId(id);
+				return {
+					income: data.income,
+					balance: data.balance,
+					spending: data.spending,
+					date: data.date,
+				};
+			})[0];
+			if (dailyArr) {
+				setDaily(parseFinanceObject(dailyArr, currency));
+			}
+		});
+		return () => {
+			unsubscribe();
+		};
+	}, [input]);
 
-    async function handleClick(id: string): Promise<void> {
-        if (input) {
-            const dailyCopy: ActiveFinanceObject = { ...daily };
-            if (id === "add-btn") {
-                const newDaily = {
-                    ...dailyCopy,
-                    income: dailyCopy.income.add(decimalConv(input)),
-                    balance: dailyCopy.balance.add(decimalConv(input)),
-                };
-                const newDailyInt = {
-                    income: newDaily.income.intValue,
-                    balance: newDaily.balance.intValue,
-                    spending: newDaily.spending.intValue,
-                    date: newDaily.date,
-                };
-                try {
-                    if (!dailyId) {
-                        await addDoc(dailyCollection, newDailyInt);
-                    } else {
-                        const dailyRef = doc(dailyCollection, dailyId);
-                        await setDoc(dailyRef, newDailyInt);
-                    }
-                } catch (err) {
-                    console.error(err);
-                }
-            } else if (id === "subtract-btn") {
-                const newDaily = {
-                    ...dailyCopy,
-                    spending: dailyCopy.spending.add(decimalConv(input)),
-                    balance: dailyCopy.balance.subtract(decimalConv(input)),
-                };
-                const newDailyInt = {
-                    income: newDaily.income.intValue,
-                    balance: newDaily.balance.intValue,
-                    spending: newDaily.spending.intValue,
-                    date: newDaily.date,
-                };
-                try {
-                    if (!dailyId) {
-                        await addDoc(dailyCollection, newDailyInt);
-                    } else {
-                        const dailyRef = await doc(dailyCollection, dailyId);
-                        await setDoc(dailyRef, newDailyInt);
-                    }
-                } catch (err) {
-                    console.error(err);
-                }
-            }
-        }
-        setInput("");
-    }
+	async function handleClick(id: string): Promise<void> {
+		if (input) {
+			const dailyCopy: ActiveFinanceObject = { ...daily };
+			if (id === "add-btn") {
+				const newDaily = {
+					...dailyCopy,
+					income: dailyCopy.income.add(decimalConv(input)),
+					balance: dailyCopy.balance.add(decimalConv(input)),
+				};
+				const newDailyInt = {
+					income: newDaily.income.intValue,
+					balance: newDaily.balance.intValue,
+					spending: newDaily.spending.intValue,
+					date: newDaily.date,
+				};
+				try {
+					if (!dailyId) {
+						await addDoc(dailyCollection, newDailyInt);
+					} else {
+						const dailyRef = doc(dailyCollection, dailyId);
+						await setDoc(dailyRef, newDailyInt);
+					}
+				} catch (err) {
+					console.error(err);
+				}
+			} else if (id === "subtract-btn") {
+				const newDaily = {
+					...dailyCopy,
+					spending: dailyCopy.spending.add(decimalConv(input)),
+					balance: dailyCopy.balance.subtract(decimalConv(input)),
+				};
+				const newDailyInt = {
+					income: newDaily.income.intValue,
+					balance: newDaily.balance.intValue,
+					spending: newDaily.spending.intValue,
+					date: newDaily.date,
+				};
+				try {
+					if (!dailyId) {
+						await addDoc(dailyCollection, newDailyInt);
+					} else {
+						const dailyRef = await doc(dailyCollection, dailyId);
+						await setDoc(dailyRef, newDailyInt);
+					}
+				} catch (err) {
+					console.error(err);
+				}
+			}
+		}
+		setInput("");
+	}
 
-    async function newDay(): Promise<void> {
-        // Add current day
-        // check to make sure this day hasn't been created yet, for example in case of
-        // connection issue in later setDoc() function which could lead to same day
-        // being stored multiple times
-        // if (last(history).date !== date) {
-        //     console.log("new day saved");
-        //     addToHistory(
-        //         income.intValue,
-        //         spending.intValue,
-        //         balance.intValue,
-        //         date
-        //     );
-        // }
-        addToHistory(
-            income.intValue,
-            spending.intValue,
-            balance.intValue,
-            date
-        );
+	async function newDay(): Promise<void> {
+		// Add current day
+		// check to make sure this day hasn't been created yet, for example in case of
+		// connection issue in later setDoc() function which could lead to same day
+		// being stored multiple times
+		if (last(history).date <= date) {
+			addToHistory(
+				income.intValue,
+				spending.intValue,
+				balance.intValue,
+				date
+			);
+		}
 
-        const timeDiff = history.length
-            ? timeDiffInDays(
-                  last(history).date,
-                  new Date().toLocaleDateString("en-CA")
-              )
-            : 1;
-        console.log(timeDiff);
-        // When more than 1 day has passed since the last use fill history with
-        // dummy days with balance kept as is for the sake of history calculations
-        if (timeDiff > 1) {
-            // Two loops to ensure out list of dates is generated correcly we can't rely on stuffing
-            // everything inside one loop and deriving it from last history object since update takes time
-            const missingDates: string[] = [];
-            console.log(missingDates);
-            let prevDay: Date;
-            for (let i = 1; i < timeDiff - 1; i++) {
-                i === 1
-                    ? (prevDay = new Date(last(history).date))
-                    : (prevDay = new Date(last(missingDates)));
-                const newDay: number = new Date().setTime(
-                    prevDay.getTime() + 86400000
-                );
-                missingDates.push(new Date(newDay).toLocaleDateString("en-CA"));
-            }
-            for (const newDate of missingDates) {
-                addToHistory(0, 0, balance.intValue, newDate);
-            }
-        }
+		const timeDiff = history.length
+			? timeDiffInDays(
+					last(history).date,
+					new Date().toLocaleDateString("en-CA")
+			  )
+			: 1;
+		// When more than 1 day has passed since the last use fill history with
+		// dummy days with balance kept as is for the sake of history calculations
+		if (timeDiff > 1) {
+			// Two loops to ensure out list of dates is generated correcly we can't rely on stuffing
+			// everything inside one loop and deriving it from last history object since update takes time
+			const missingDates: string[] = [];
+			let prevDay: Date;
+			for (let i = 1; i < timeDiff; i++) {
+				i === 1
+					? (prevDay = new Date(date))
+					: (prevDay = new Date(last(missingDates)));
+				const newDay: number = new Date().setTime(
+					prevDay.getTime() + 86400000
+				);
+				missingDates.push(new Date(newDay).toLocaleDateString("en-CA"));
+			}
+			for (const newDate of missingDates) {
+				addToHistory(0, 0, balance.intValue, newDate);
+			}
+		}
 
-        // onSnapshot(historyCollection, (snapshot) => {
-        //     const history: storedFinanceObject[] =
-        //         snapshot.docs.map<storedFinanceObject>((doc) => {
-        //             const data = doc.data();
-        //             console.log("Data pulled");
-        //             return {
-        //                 income: data.income,
-        //                 spending: data.spending,
-        //                 balance: data.balance,
-        //                 date: data.date,
-        //             };
-        //         });
-        //     const timeDiff = history.length
-        //         ? timeDiffInDays(
-        //               last(history).date,
-        //               new Date().toLocaleDateString("en-CA")
-        //           )
-        //         : 1;
-        //     console.log(timeDiff);
-        //     // When more than 1 day has passed since the last use fill history with
-        //     // dummy days with balance kept as is for the sake of history calculations
-        //     if (timeDiff > 1) {
-        //         // Two loops to ensure out list of dates is generated correcly we can't rely on stuffing
-        //         // everything inside one loop and deriving it from last history object since update takes time
-        //         const missingDates: string[] = [];
-        //         console.log(missingDates);
-        //         let prevDay: Date;
-        //         for (let i = 1; i < timeDiff - 1; i++) {
-        //             i === 1
-        //                 ? (prevDay = new Date(last(history).date))
-        //                 : (prevDay = new Date(last(missingDates)));
-        //             const newDay: number = new Date().setTime(
-        //                 prevDay.getTime() + 86400000
-        //             );
-        //             missingDates.push(
-        //                 new Date(newDay).toLocaleDateString("en-CA")
-        //             );
-        //         }
-        //         for (const newDate of missingDates) {
-        //             addToHistory(0, 0, last(history).balance, newDate);
-        //         }
-        //     }
-        // });
+		const dailyRef = doc(dailyCollection, dailyId);
+		const dailyCopy: ActiveFinanceObject = { ...daily };
+		const newDaily: storedFinanceObject = {
+			income: 0,
+			balance: dailyCopy.balance.intValue,
+			spending: 0,
+			date: new Date().toLocaleDateString("en-CA"),
+		};
+		await setDoc(dailyRef, newDaily);
+	}
 
-        const dailyRef = doc(dailyCollection, dailyId);
-        const dailyCopy: ActiveFinanceObject = { ...daily };
-        const newDaily: storedFinanceObject = {
-            income: 0,
-            balance: dailyCopy.balance.intValue,
-            spending: 0,
-            date: new Date().toLocaleDateString("en-CA"),
-        };
-        await setDoc(dailyRef, newDaily);
-    }
+	useEffect(() => {
+		if (date && new Date().toLocaleDateString("en-CA") > date) {
+			newDay();
+		}
+	}, [daily]);
 
-    useEffect(() => {
-        if (date && new Date().toLocaleDateString("en-CA") > date) {
-            newDay();
-        }
-    }, [daily]);
+	const graphTextDaily: GraphText = {
+		income: "Daily income",
+		balance: "Total balance",
+		spending: "Daily spending",
+	};
 
-    const graphTextDaily: GraphText = {
-        income: "Daily income",
-        balance: "Total balance",
-        spending: "Daily spending",
-    };
+	const formattedDateDaily = {
+		...daily,
+		date: new Date(date).toLocaleDateString(),
+	};
 
-    const formattedDateDaily = {
-        ...daily,
-        date: new Date(date).toLocaleDateString(),
-    };
-
-    return (
-        <div className="daily">
-            <RenderGraph
-                data={formattedDateDaily}
-                currency={currency}
-                graphText={graphTextDaily}
-            />
-            <input
-                placeholder="Change balance"
-                aria-label="Change balance"
-                className="daily-input"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-            ></input>
-            <div className="button-container">
-                <button
-                    className="daily-btn"
-                    id="add-btn"
-                    onClick={(e) => handleClick(e.currentTarget.id)}
-                >
-                    Add
-                </button>
-                <button
-                    className="daily-btn"
-                    id="subtract-btn"
-                    onClick={(e) => handleClick(e.currentTarget.id)}
-                >
-                    Subtract
-                </button>
-            </div>
-        </div>
-    );
+	return (
+		<div className="daily">
+			<RenderGraph
+				data={formattedDateDaily}
+				currency={currency}
+				graphText={graphTextDaily}
+			/>
+			<input
+				placeholder="Change balance"
+				aria-label="Change balance"
+				className="daily-input"
+				value={input}
+				onChange={(e) => setInput(e.target.value)}
+			></input>
+			<div className="button-container">
+				<button
+					className="daily-btn"
+					id="add-btn"
+					onClick={(e) => handleClick(e.currentTarget.id)}
+				>
+					Add
+				</button>
+				<button
+					className="daily-btn"
+					id="subtract-btn"
+					onClick={(e) => handleClick(e.currentTarget.id)}
+				>
+					Subtract
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Daily;
--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,206 +1,216 @@
 import { FC, ReactNode, useEffect, useState } from "react";
 import {
-    storedFinanceObject,
-    CurrencyObject,
-    DateRange,
-    ActiveFinanceObject,
-    DateTuple,
-    GraphText,
+	storedFinanceObject,
+	CurrencyObject,
+	DateRange,
+	ActiveFinanceObject,
+	DateTuple,
+	GraphText,
 } from "../utils/interfaces";
 import Graph from "./Graph";
 import {
-    last,
-    CalcAverage,
-    dateToLocale,
-    constructEmptyFinance,
-    getPastDate,
+	last,
+	CalcAverage,
+	dateToLocale,
+	constructEmptyFinance,
+	getPastDate,
 } from "../utils/utils";
 import { v4 as uuid } from "uuid";
 import { parseFinanceObject } from "../utils/currencies";
 import "../styles/history.css";
 
 interface HistoryProps {
-    history: storedFinanceObject[];
-    currency: CurrencyObject;
+	history: storedFinanceObject[];
+	currency: CurrencyObject;
 }
 
 const History: FC<HistoryProps> = (props) => {
-    const { history, currency } = props;
-    const [date, setDate] = useState<DateRange>({
-        startDate: "",
-        endDate: "",
-    });
+	const { history, currency } = props;
+	const [date, setDate] = useState<DateRange>({
+		startDate: "",
+		endDate: "",
+	});
 
-    const { startDate, endDate } = date;
+	const sortedHistory = history.sort((a, b) => {
+		if (a.date < b.date) {
+			return -1;
+		} else if (a.date > b.date) {
+			return 1;
+		} else {
+			return 0;
+		}
+	});
 
-    const [data, setData] = useState<ActiveFinanceObject>(
-        constructEmptyFinance(currency, "")
-    );
+	const { startDate, endDate } = date;
 
-    // State to keep track of whether shortcut was clicked
-    const [shortcutUsed, setShortcutUsed] = useState(false);
+	const [data, setData] = useState<ActiveFinanceObject>(
+		constructEmptyFinance(currency, "")
+	);
 
-    function handleChange(target: HTMLInputElement): void {
-        if (target.name === "start-date") {
-            setDate((prevDate) => ({ ...prevDate, startDate: target.value }));
-        } else if (target.name === "end-date") {
-            setDate((prevDate) => ({ ...prevDate, endDate: target.value }));
-        }
-    }
+	// State to keep track of whether shortcut was clicked
+	const [shortcutUsed, setShortcutUsed] = useState(false);
 
-    function handleClick(): void {
-        const trimmedHistory: storedFinanceObject[] = history.filter(
-            (historyObj) =>
-                historyObj.date >= startDate && historyObj.date <= endDate
-        );
-        const averageIncome = CalcAverage(trimmedHistory, "income");
-        const averageBalance = CalcAverage(trimmedHistory, "balance");
-        const averageSpending = CalcAverage(trimmedHistory, "spending");
-        const formattedDate = dateToLocale([startDate, endDate]);
+	function handleChange(target: HTMLInputElement): void {
+		if (target.name === "start-date") {
+			setDate((prevDate) => ({ ...prevDate, startDate: target.value }));
+		} else if (target.name === "end-date") {
+			setDate((prevDate) => ({ ...prevDate, endDate: target.value }));
+		}
+	}
 
-        setData(
-            parseFinanceObject(
-                {
-                    income: averageIncome,
-                    balance: averageBalance,
-                    spending: averageSpending,
-                    date: `${formattedDate[0]} - ${formattedDate[1]}`,
-                },
-                currency
-            )
-        );
-    }
+	function handleClick(): void {
+		const trimmedHistory: storedFinanceObject[] = sortedHistory.filter(
+			(historyObj) =>
+				historyObj.date >= startDate && historyObj.date <= endDate
+		);
+		const averageIncome = CalcAverage(trimmedHistory, "income");
+		const averageBalance = CalcAverage(trimmedHistory, "balance");
+		const averageSpending = CalcAverage(trimmedHistory, "spending");
+		const formattedDate = dateToLocale([startDate, endDate]);
 
-    function handleShortcut(target: HTMLButtonElement): void {
-        const text = target.name;
-        setShortcutUsed(true);
-        if (text === "Yesterday") {
-            setDate({
-                startDate: last(history).date,
-                endDate: last(history).date,
-            });
-        } else {
-            let days: number = 0;
-            if (text === "7D") days = 7;
-            else if (text === "14D") days = 14;
-            else if (text === "30D") days = 30;
-            else if (text === "6M") days = 183; // 182.625
-            else days = 365;
-            const dates: DateTuple = getPastDate(history, days);
-            setDate({
-                startDate: dates[0],
-                endDate: dates[1],
-            });
-        }
-    }
+		setData(
+			parseFinanceObject(
+				{
+					income: averageIncome,
+					balance: averageBalance,
+					spending: averageSpending,
+					date: `${formattedDate[0]} - ${formattedDate[1]}`,
+				},
+				currency
+			)
+		);
+	}
 
-    // Graph should auto-render when the shortcut is clicked, but useState
-    // is asynchronous so calling handleClick() inside is not reliable
-    // therefore we need to make sure state has updated before we run it
-    // but we also want to only render it with shortcuts, not when used
-    // uses date selector. Solution would be a custom useState hook that
-    // runs function after state update, but using conditional useEffect is
-    // easier solution, although it requires another state to keep track of whether
-    // shortcut was clicked or not
+	function handleShortcut(target: HTMLButtonElement): void {
+		const text = target.name;
+		setShortcutUsed(true);
+		if (text === "1D") {
+			setDate({
+				startDate: last(sortedHistory).date,
+				endDate: last(sortedHistory).date,
+			});
+		} else {
+			let days: number = 0;
+			if (text === "7D") days = 7;
+			else if (text === "14D") days = 14;
+			else if (text === "30D") days = 30;
+			else if (text === "6M") days = 183; // 182.625
+			else days = 365;
+			const dates: DateTuple = getPastDate(history, days);
+			setDate({
+				startDate: dates[0],
+				endDate: dates[1],
+			});
+		}
+	}
 
-    useEffect(() => {
-        if (shortcutUsed) {
-            setShortcutUsed(false);
-            handleClick();
-        }
-    }, [date]);
-    console.log(data.date);
-    function renderGraph(): ReactNode {
-        if (!history.length) {
-            return (
-                <div className="date-error-msg">
-                    <h3>You don't have any history yet.</h3>
-                </div>
-            );
-        } else if (!data.date) {
-            return <></>;
-        } else if (
-            startDate < history[0].date ||
-            endDate > last(history).date
-        ) {
-            return (
-                <div className="date-error-msg">
-                    <h3>
-                        One or both dates are out of bounds. Please select a
-                        different date.
-                    </h3>
-                </div>
-            );
-        } else {
-            return (
-                <Graph
-                    data={data}
-                    currency={currency}
-                    graphText={graphTextHistory}
-                />
-            );
-        }
-    }
+	// Graph should auto-render when the shortcut is clicked, but useState
+	// is asynchronous so calling handleClick() inside is not reliable
+	// therefore we need to make sure state has updated before we run it
+	// but we also want to only render it with shortcuts, not when used
+	// uses date selector. Solution would be a custom useState hook that
+	// runs function after state update, but using conditional useEffect is
+	// easier solution, although it requires another state to keep track of whether
+	// shortcut was clicked or not
 
-    function renderShortcuts(): ReactNode {
-        const shortcutText: string[] = ["1D", "7D", "14D", "30D", "6M", "1Y"];
-        const shortcutElements: ReactNode[] = shortcutText.map((text) => (
-            <button
-                key={uuid()}
-                className="history-shortcut"
-                name={text}
-                disabled={!history.length}
-                onClick={(e) => handleShortcut(e.currentTarget)}
-            >
-                {text}
-            </button>
-        ));
-        return shortcutElements;
-    }
+	useEffect(() => {
+		if (shortcutUsed) {
+			setShortcutUsed(false);
+			handleClick();
+		}
+	}, [date]);
 
-    const graphTextHistory: GraphText = {
-        income: "Average income",
-        balance: "Average balance",
-        spending: "Average spending",
-    };
+	function renderGraph(): ReactNode {
+		if (!history.length) {
+			return (
+				<div className="date-error-msg">
+					<h3>You don't have any history yet.</h3>
+				</div>
+			);
+		} else if (!data.date) {
+			return <></>;
+		} else if (
+			startDate < history[0].date ||
+			endDate > last(history).date
+		) {
+			return (
+				<div className="date-error-msg">
+					<h3>
+						One or both dates are out of bounds. Please select a
+						different date.
+					</h3>
+				</div>
+			);
+		} else {
+			return (
+				<Graph
+					data={data}
+					currency={currency}
+					graphText={graphTextHistory}
+				/>
+			);
+		}
+	}
 
-    const minDate: string = history.length ? history[0].date : "";
-    const maxDate: string = history.length ? last(history).date : "";
+	function renderShortcuts(): ReactNode {
+		const shortcutText: string[] = ["1D", "7D", "14D", "30D", "6M", "1Y"];
+		const shortcutElements: ReactNode[] = shortcutText.map((text) => (
+			<button
+				key={uuid()}
+				className="history-shortcut"
+				name={text}
+				disabled={!history.length}
+				onClick={(e) => handleShortcut(e.currentTarget)}
+			>
+				{text}
+			</button>
+		));
+		return shortcutElements;
+	}
 
-    return (
-        <div className="history">
-            <h2 className="title">History</h2>
-            {renderGraph()}
-            <div className="shortcut-container">{renderShortcuts()}</div>
-            <label className="date-label">
-                Start date:
-                <input
-                    type="date"
-                    name="start-date"
-                    value={startDate}
-                    min={minDate}
-                    max={maxDate}
-                    disabled={!history.length}
-                    onChange={(e) => handleChange(e.target)}
-                ></input>
-            </label>
-            <label className="date-label">
-                End date:
-                <input
-                    type="date"
-                    name="end-date"
-                    value={endDate}
-                    min={minDate}
-                    max={maxDate}
-                    disabled={!history.length}
-                    onChange={(e) => handleChange(e.target)}
-                ></input>
-            </label>
-            <button disabled={!history.length} onClick={handleClick}>
-                Select date
-            </button>
-        </div>
-    );
+	const graphTextHistory: GraphText = {
+		income: "Average income",
+		balance: "Average balance",
+		spending: "Average spending",
+	};
+
+	const minDate: string = history.length ? sortedHistory[0].date : "";
+	const maxDate: string = history.length ? last(sortedHistory).date : "";
+
+	return (
+		<div className="history">
+			<h2 className="title">History</h2>
+			{renderGraph()}
+			<div className="shortcut-container">{renderShortcuts()}</div>
+			<label className="date-label">
+				Start date:
+				<input
+					type="date"
+					name="start-date"
+					value={startDate}
+					min={minDate}
+					max={maxDate}
+					disabled={!history.length}
+					onChange={(e) => handleChange(e.target)}
+				></input>
+			</label>
+			<label className="date-label">
+				End date:
+				<input
+					type="date"
+					name="end-date"
+					value={endDate}
+					min={minDate}
+					max={maxDate}
+					disabled={!history.length}
+					onChange={(e) => handleChange(e.target)}
+				></input>
+			</label>
+			<button disabled={!history.length} onClick={handleClick}>
+				Select date
+			</button>
+		</div>
+	);
 };
 
 export default History;

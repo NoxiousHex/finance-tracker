fc0ad2e82f2b3ffe3dab370dd937af511127e75c
NoxiousHex <131687569+NoxiousHex@users.noreply.github.com>
2023-07-19 23:04:06 +0200

    Make graph amounts always render on a new line

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,57 +1,59 @@
 import { FC } from "react";
 import { calculatePercent, max } from "../utils/utils";
 import {
-    GraphText,
-    CurrencyObject,
-    ActiveFinanceObject,
+	GraphText,
+	CurrencyObject,
+	ActiveFinanceObject,
 } from "../utils/interfaces";
 import "../styles/graph.css";
 interface GraphProps {
-    data: ActiveFinanceObject;
-    currency: CurrencyObject;
-    graphText: GraphText;
+	data: ActiveFinanceObject;
+	currency: CurrencyObject;
+	graphText: GraphText;
 }
 
 const Graph: FC<GraphProps> = (props) => {
-    const { data, graphText } = props;
-    const { income, balance, spending, date } = data;
-    const graphNumbers = [income.intValue, balance.intValue, spending.intValue];
-    // Get highest value to scale graphs towards to prevent overflow
-    const maximum: number = max(graphNumbers);
-    const incomeScale: number = calculatePercent(graphNumbers[0], maximum) || 0;
-    const balanceScale: number =
-        calculatePercent(graphNumbers[1], maximum) || 0;
-    const spendingScale: number =
-        calculatePercent(graphNumbers[2], maximum) || 0;
+	const { data, graphText } = props;
+	const { income, balance, spending, date } = data;
+	const graphNumbers = [income.intValue, balance.intValue, spending.intValue];
+	// Get highest value to scale graphs towards to prevent overflow
+	const maximum: number = max(graphNumbers);
+	const incomeScale: number = calculatePercent(graphNumbers[0], maximum) || 0;
+	const balanceScale: number =
+		calculatePercent(graphNumbers[1], maximum) || 0;
+	const spendingScale: number =
+		calculatePercent(graphNumbers[2], maximum) || 0;
 
-    return (
-        <div className="graph-body">
-            <h2 className="graph-date">{date}</h2>
-            <div className="graph-container">
-                <h3 className="graph-text">{`${
-                    graphText.income
-                }: ${income.format()}`}</h3>
-                <div
-                    style={{ height: `${income ? incomeScale : 0}%` }}
-                    className="income-graph"
-                ></div>
-                <h3 className="graph-text">{`${
-                    graphText.balance
-                }: ${balance.format()}`}</h3>
-                <div
-                    style={{ height: `${balance ? balanceScale : 0}%` }}
-                    className="balance-graph"
-                ></div>
-                <h3 className="graph-text">{`${
-                    graphText.spending
-                }: ${spending.format()}`}</h3>
-                <div
-                    style={{ height: `${spending ? spendingScale : 0}%` }}
-                    className="spending-graph"
-                ></div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="graph-body">
+			<h2 className="graph-date">{date}</h2>
+			<div className="graph-container">
+				<h3 className="graph-text">
+					{`${graphText.income}:`} <span>{`${income.format()}`}</span>
+				</h3>
+				<div
+					style={{ height: `${income ? incomeScale : 0}%` }}
+					className="income-graph"
+				></div>
+				<h3 className="graph-text">
+					{`${graphText.balance}:`}{" "}
+					<span>{`${balance.format()}`}</span>
+				</h3>
+				<div
+					style={{ height: `${balance ? balanceScale : 0}%` }}
+					className="balance-graph"
+				></div>
+				<h3 className="graph-text">
+					{`${graphText.spending}:`}{" "}
+					<span>{`${spending.format()}`}</span>
+				</h3>
+				<div
+					style={{ height: `${spending ? spendingScale : 0}%` }}
+					className="spending-graph"
+				></div>
+			</div>
+		</div>
+	);
 };
 
 export default Graph;

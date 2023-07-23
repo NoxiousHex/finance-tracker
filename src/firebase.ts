eb0ed8074df03eb81acd724e18dd493c95f819f0
NoxiousHex <131687569+NoxiousHex@users.noreply.github.com>
2023-07-23 20:53:05 +0200

    Implement using ENV vars to store Firebase info

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,16 @@ import { DatabaseConfig } from "./utils/interfaces";
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
 
+const { VITE_API_KEY, VITE_PROJECT_ID, VITE_MESSAGING_SENDER_ID, VITE_APP_ID } =
+	import.meta.env;
+
 const firebaseConfig: DatabaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
+	apiKey: VITE_API_KEY,
+	authDomain: `${VITE_PROJECT_ID}.firebaseapp.com`,
+	projectId: VITE_PROJECT_ID,
+	storageBucket: `${VITE_PROJECT_ID}.appspot.com`,
+	messagingSenderId: VITE_MESSAGING_SENDER_ID,
+	appId: VITE_APP_ID,
 };
 
 // Initialize Firebase
